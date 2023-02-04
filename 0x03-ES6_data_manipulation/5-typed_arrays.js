69b4ca8b5812b68283a1b850869731b6fd18c31d
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-04 18:39:45 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw Error('Position outside range')
-    }
-    const ArrBuffer = new ArrayBuffer(length);
-    const int8view = new Int8Array(ArrBuffer, 0, length);
-    int8view.set(value, position);
-    return new DataView(ArrBuffer);
+  if (position < 0 || position >= length) {
+      throw Error('Position outside range');
+  }
+  const ArrBuffer = new ArrayBuffer(length);
+  const int8view = new Int8Array(ArrBuffer, 0, length);
+  int8view.set([value], position);
+  return new DataView(ArrBuffer);
 }
