a245e78a5dde158e8040c9fcba36a5469621ed2b
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-04 18:37:32 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
     const ArrBuffer = new ArrayBuffer(length);
     const int8view = new Int8Array(ArrBuffer, 0, length);
     int8view.set(value, position);
-    return new DataView(int8view);
+    return new DataView(ArrBuffer);
 }
