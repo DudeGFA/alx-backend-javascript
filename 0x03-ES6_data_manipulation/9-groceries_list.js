4aafca9bf09b14483ac34c1866af50721641e7b9
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-04 23:21:52 +0100

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
 export default function groceriesList() {
-    return new Map(object.entries({
+    const obj = {
         'Apples': 10,
         'Tomatoes': 10,
         'Pasta': 1,
         'Rice': 1,
         'Banana': 5
-      }));
+      }
+    return new Map(object.entries(obj));
 }
