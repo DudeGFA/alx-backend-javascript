db5e4479c89d228ff5c2e1d71078a3ed4c65d646
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-04 18:07:08 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,13 @@
 export default function updateStudentGradeByCity(StuList, city, newGrades) {
-    let filteredArr = StuList.filter((obj) => obj.location === city);
-    filteredArr.map((stu) => stu.grade = newGrades.filter((Grade) => Grade.studentId === stu.id).grade);
-    return filteredArr
+  if (!Array.isArray(StuList) || !Array.isArray(newGrades)) {
+    return []
+  }
+
+  let filteredArr = StuList.filter((obj) => obj.location === city);
+  filteredArr.map((stu) =>
+    {student = newGrades.find((Grade) => Grade.studentId === stu.id);
+    student != undefined ? stu.grade  = student.grade: stu.grade = 'N/A'
+    return stu;
+});
+  return filteredArr
 }
