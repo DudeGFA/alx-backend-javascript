e21db13fc66030be7edba8aa6e27f9fdf4f3e690
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-05 00:46:22 +0100

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
 export const weakMap = new WeakMap();
 
 export default function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) === undefined) {
-    weakMap.set(endpoint, 0);
+  if (!weakMap.get(endpoint)) {
+    weakMap.set(endpoint, 1);
   } else { 
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
-  if ((weakMap.get(endpoint)) >= 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw Error('Endpoint load is high');
   }
 }
