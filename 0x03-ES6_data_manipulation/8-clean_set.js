3dcc0fe3718bc06b89e60cf1baa594585efc6ec7
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-04 22:55:54 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(aset, startString) {
   const newArr = [];
   if (startString === undefined || startString.length === 0) {
-    return newArr;
+    return '';
   }
   for (const item of aset) {
     if (item.startsWith(startString)) {
