952eafc5dbd23e213217fb513889442492bdf8a7
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-05 00:04:27 +0100

    update 10-update_uniq_item.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(items) {
   if (!(items instanceof Map)) {
-    throw Error('Cannot process')
+    throw Error('Cannot process');
   }
-  for (const key in items.keys()) {
+  for (const key of items.keys()) {
     if (items.get(key) === 1) {
       items.set(key, 100);
     }
