b1a2cb2e2320d58dde73250fc5518b402236b6ac
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-20 21:44:52 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
-console.log("Welcome to Holberton School, what is your name?")
-process.stdin.on('readable', function() {
-  var input = process.stdin.read();
-  if (input !== null) {
-    console.log("Your name is: " + input);
-  }
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
+process.stdin.on('data', function(data) {
+  process.stdout.write('Your name is: ' + data.toString())
+  process.exit()
 });
-process.on('SIGINT', function(code) {
-console.log('This important software is now closing');
-process.exit()
+process.on('exit', () => {
+process.stdout.write('This important software is now closing\n');
 });
